71824a08a34c3fccc35517c7ffc783aa372e0921
Nushio <Nushio@7f3cc5be-d49c-11de-bb02-e302c5db49d5>
2009-11-23 16:50:00 +0000

    Initial code commit for the ShoddyTCGServer. Code comes in great part from Pokenet's Server code, done by Sienide.

---
+++ ShoddyTCGServer/src/org/pokenet/server/backend/entity/SupporterCard.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.pokenet.server.backend.entity;
+
+/**
+ * @author Nushio
+ *
+ */
+public class SupporterCard extends CardType{
+	public enum Type { BASIC, SPECIAL}
+	
+	private String Text;
+
+	/**
+	 * @return the text
+	 */
+	public String getText() {
+		return Text;
+	}
+
+	/**
+	 * @param text the text to set
+	 */
+	public void setText(String text) {
+		Text = text;
+	}
+}
