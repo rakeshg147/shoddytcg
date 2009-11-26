6fdd8f2030da637e01aeffff2489e1198369b687
Nushio <Nushio@7f3cc5be-d49c-11de-bb02-e302c5db49d5>
2009-11-26 17:27:21 +0000

    Parser now eats through EnergyCards too. Added some Trainer Types.

--- ShoddyTCGServer/src/com/shoddytcg/server/backend/entity/TrainerCard.java
+++ ShoddyTCGServer/src/com/shoddytcg/server/backend/entity/TrainerCard.java
@@ -3,14 +3,31 @@
  */
 package com.shoddytcg.server.backend.entity;
 
+
 /**
  * @author Nushio
  *
  */
 public class TrainerCard extends CardType{
-	public enum Type { TRAINER, TOOL}
+	public enum Type { TRAINER, TOOL, TM, POKEMON}
 	
+	private Type type;
 	private String Text;
+
+	
+	/**
+	 * @return the type
+	 */
+	public Type getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(Type type) {
+		this.type = type;
+	}
 
 	/**
 	 * @return the text
@@ -26,5 +43,18 @@ public class TrainerCard extends CardType{
 		Text = text;
 	}
 	
-	
+	public static Type returnType(String type){
+		try{
+			if(type.equalsIgnoreCase("Tool"))
+				return Type.TOOL;
+			else if(type.equalsIgnoreCase("TM"))
+				return Type.TM;
+			else if(type.equalsIgnoreCase("Pokemon"))
+				return Type.POKEMON;
+			else 
+				return Type.TRAINER;
+		}catch(Exception e){
+			return Type.TRAINER;
+		}
+	}
 }
