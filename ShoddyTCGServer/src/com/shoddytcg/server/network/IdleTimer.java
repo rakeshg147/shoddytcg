ffa0c29bb8f2d9b379aefeeb5942f156bd50527f
Nushio <Nushio@7f3cc5be-d49c-11de-bb02-e302c5db49d5>
2009-11-30 04:03:06 +0000

    Everything's legal now. Added AGPL notice for all files, and BSD License for FileListing.

--- ShoddyTCGServer/src/com/shoddytcg/server/network/IdleTimer.java
+++ ShoddyTCGServer/src/com/shoddytcg/server/network/IdleTimer.java
@@ -1,7 +1,24 @@
+/**
+ *  Copyright (C) 2009 ShoddyTCG Developer Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.shoddytcg.server.network;
 
 /**
- * A class which kicks players if they've been idle for too long
+ * A class which kicks players if they've been idle for too long. 
+ * Hope to not use it. 
  */
 public class IdleTimer implements Runnable {
 	private boolean m_isRunning = false;
--- ShoddyTCGServer/src/com/shoddytcg/server/network/TcpProtocolHandler.java
+++ ShoddyTCGServer/src/com/shoddytcg/server/network/TcpProtocolHandler.java
@@ -1,3 +1,19 @@
+/**
+ *  Copyright (C) 2009 ShoddyTCG Developer Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.shoddytcg.server.network;
 
 import java.io.IOException;
@@ -13,7 +29,7 @@ import com.shoddytcg.server.backend.entity.Card;
 import com.shoddytcg.server.backend.entity.Deck;
 import com.shoddytcg.server.backend.entity.PlayerChar;
 import com.shoddytcg.server.backend.entity.PlayerChar.RequestType;
-import com.shoddytcg.server.network.message.PokenetMessage;
+import com.shoddytcg.server.network.message.ShoddyMessage;
 import com.shoddytcg.server.network.message.RequestMessage;
 
 /**
@@ -321,7 +337,7 @@ public class TcpProtocolHandler extends IoHandlerAdapter {
 	 * @param session
 	 * @param m
 	 */
-	public static void writeMessage(IoSession session, PokenetMessage m) {
+	public static void writeMessage(IoSession session, ShoddyMessage m) {
 		session.write(m.getMessage());
 	}
 }
